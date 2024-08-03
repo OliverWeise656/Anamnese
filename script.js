7f90158d6ec23a7ed5c776a96d1d23d180af3ea8
Glitch (anamnese-und-hoertest) <none>
2024-08-03 14:20:52 +0000

    🌷🌅 Checkpoint ./script.js:68423411/2

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ async function sendMessage() {
             } else if (state.voiceAnalysisRecommended) {
                 window.location.href = 'https://classic-broadleaf-blender.glitch.me';
             }
-        }, 3000); // 3 Sekunden Verzögerung
+        }, 6000); // 3 Sekunden Verzögerung
     }
 }
 
