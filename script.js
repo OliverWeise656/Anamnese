1b0397c98211a31d94ad9ec052156e124701841a
Glitch (anamnese-und-hoertest) <none>
2024-08-23 00:15:23 +0000

    ⛲️🚎 Checkpoint ./script.js:68423411/83

--- script.js
+++ script.js
@@ -933,7 +933,7 @@ function saveResultsAsPDF() {
                     window.location.href = 'https://mottier-test.glitch.me';
                 } else {
                     setTimeout(() => {
-                        alert('Herzlichen Dank für Ihre Unterstützung! Auf Ihrem Desktop wurde die folgende PDF abgelegt: ' + fileName + '. Auf diese Weise tragen Sie zu einem reibungslosen Ablauf Ihres Besuchs bei uns bei! Bitte Beachten Sie, dass das Ergebnis des Hörtests besonderer Interpretation bedarf und nur für den Gebrauch in unserer Praxis vorgesehen ist.');
+                        alert('Herzlichen Dank für Ihre Unterstützung! Auf Ihrem Desktop wurde die folgende PDF abgelegt: ' + fileName + '. Bitte bringen Sie diese ausgedruckt zu Ihrem Besuch bei uns mit. Auf diese Weise tragen Sie zu einem reibungslosen Ablauf Ihres Besuchs bei uns bei! Bitte Beachten Sie, dass das Ergebnis des Hörtests besonderer Interpretation bedarf und nur für den Gebrauch in unserer Praxis vorgesehen ist.');
                     }, 5000);
                 }
             }, 5000);
